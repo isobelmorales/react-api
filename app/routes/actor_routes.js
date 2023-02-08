8176f8a53f0a816e6730c39cf76a0d18086495f2
isobelmorales <isofmorales@gmail.com>
2023-02-08 13:27:35 -0500

    Users can now delete actors

--- app/routes/actor_routes.js
+++ app/routes/actor_routes.js
@@ -45,9 +45,19 @@ router.patch('/actors/:movieId/:actorId', requireToken, removeBlanks, (req, res,
 
 // DESTROY
 // DELETE /actors/:movieId/:actorId
-// router.delete('/actors/:movieId/:actorId', requireToken, (req, res, next) => {
-// 	const movieId = req.params.movieId
-//  const actorId = req.params.actorId
-// })
+router.delete('/actors/:movieId/:actorId', requireToken, (req, res, next) => {
+	const movieId = req.params.movieId
+    const actorId = req.params.actorId
+    Movie.findById(movieId)
+        .then(handle404)
+        .then(movie => {
+            const theActor = movie.actors.id(actorId)
+            requireOwnership(req, movie)
+            theActor.remove()
+            return movie.save()
+        })
+        .then(() => res.sendStatus(204))
+        .catch(next)
+})
 
 module.exports = router
